4fd4b0205ff3fcf14ac62d9dc755b127d5165409
Samuel Attard <sattard@slack-corp.com>
2019-01-30 14:38:44 -0800

    fix: handle missing artifacts

--- src/circleci/artifacts.ts
+++ src/circleci/artifacts.ts
@@ -9,10 +9,15 @@ export async function getCircleArtifacts (context: IContext, buildNumber: number
     `https://circleci.com/api/v1.1/project/github/${REPO_SLUG}/${buildNumber}/artifacts?circle-token=${CIRCLE_TOKEN}`
   );
   const artifactList = await response.json();
+  const missing: string[] = [];
 
   async function getArtifact (name: string) {
     context.logger.info(`fetching artifact "${name}" for build:`, `${buildNumber}`);
     const circleArtifact = artifactList.find(artifact => artifact.path.endsWith(name));
+    if (!circleArtifact) {
+      missing.push(name);
+      return null;
+    }
 
     const contentResponse = await fetch(`${circleArtifact.url}?circle-token=${CIRCLE_TOKEN}`);
     return await contentResponse.text();
@@ -25,6 +30,7 @@ export async function getCircleArtifacts (context: IContext, buildNumber: number
   ])
 
   return {
+    missing,
     new: contents[0],
     old: contents[1],
     oldDigSpot: contents[2].trim(),
